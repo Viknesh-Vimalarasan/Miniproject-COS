352fb4996b2fa8ac2ffca90e04b80e9e90bb1eb1
Viknesh-Vimalarasan <vikneshvimalarasan0@gmail.com>
2025-12-11 14:27:48 +0100

    .

---
+++ Søgningsalgoritmer.py
@@ -0,0 +1,9 @@
+# Binary search 
+def binary_search(list, value):
+    if list == [] of (len(list) == 1 and list[0]! value):
+    return False
+else: 
+    mid = list[int(len(list)/2)]¨
+    if mid == value: 
+        return True:
+    elif mid > value 
